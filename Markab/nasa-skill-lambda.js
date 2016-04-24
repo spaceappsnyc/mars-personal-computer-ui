25dbc4694ff45621d71a25c66a64b700fbda6d3a
Pawel Raszewski <pawel.raszewski@teamaol.com>
2016-04-23 20:26:30 -0400

    fixes

---
+++ Markab/nasa-skill-lambda.js
@@ -0,0 +1,336 @@
+/**
+ Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+ http://aws.amazon.com/apache2.0/
+
+ or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
+'use strict';
+
+function AlexaSkill(appId) {
+    this._appId = appId;
+}
+
+AlexaSkill.speechOutputType = {
+    PLAIN_TEXT: 'PlainText',
+    SSML: 'SSML'
+}
+
+AlexaSkill.prototype.requestHandlers = {
+    LaunchRequest: function (event, context, response) {
+        this.eventHandlers.onLaunch.call(this, event.request, event.session, response);
+    },
+    IntentRequest: function (event, context, response) {
+        this.eventHandlers.onIntent.call(this, event.request, event.session, response);
+    },
+    SessionEndedRequest: function (event, context) {
+        this.eventHandlers.onSessionEnded(event.request, event.session);
+        context.succeed();
+    }
+};
+
+/**
+ * Override any of the eventHandlers as needed
+ */
+AlexaSkill.prototype.eventHandlers = {
+    /**
+     * Called when the session starts.
+     * Subclasses could have overriden this function to open any necessary resources.
+     */
+    onSessionStarted: function (sessionStartedRequest, session) {
+    },
+    /**
+     * Called when the user invokes the skill without specifying what they want.
+     * The subclass must override this function and provide feedback to the user.
+     */
+    onLaunch: function (launchRequest, session, response) {
+        throw "onLaunch should be overriden by subclass";
+    },
+    /**
+     * Called when the user specifies an intent.
+     */
+    onIntent: function (intentRequest, session, response) {
+        var intent = intentRequest.intent,
+            intentName = intentRequest.intent.name,
+            intentHandler = this.intentHandlers[intentName];
+        if (intentHandler) {
+            console.log('dispatch intent = ' + intentName);
+            intentHandler.call(this, intent, session, response);
+        } else {
+            throw 'Unsupported intent = ' + intentName;
+        }
+    },
+    /**
+     * Called when the user ends the session.
+     * Subclasses could have overriden this function to close any open resources.
+     */
+    onSessionEnded: function (sessionEndedRequest, session) {
+    }
+};
+
+/**
+ * Subclasses should override the intentHandlers with the functions to handle specific intents.
+ */
+AlexaSkill.prototype.intentHandlers = {};
+
+AlexaSkill.prototype.execute = function (event, context) {
+    try {
+        console.log("session applicationId: " + event.session.application.applicationId);
+
+// Validate that this request originated from authorized source.
+        if (this._appId && event.session.application.applicationId !== this._appId) {
+            console.log("The applicationIds don't match : " + event.session.application.applicationId + " and "
+                + this._appId);
+            throw "Invalid applicationId";
+        }
+
+        if (!event.session.attributes) {
+            event.session.attributes = {};
+        }
+
+        if (event.session.new) {
+            this.eventHandlers.onSessionStarted(event.request, event.session);
+        }
+
+// Route the request to the proper handler which may have been overriden.
+        var requestHandler = this.requestHandlers[event.request.type];
+        requestHandler.call(this, event, context, new Response(context, event.session));
+    } catch (e) {
+        console.log("Unexpected exception " + e);
+        context.fail(e);
+    }
+};
+
+var Response = function (context, session) {
+    this._context = context;
+    this._session = session;
+};
+
+function createSpeechObject(optionsParam) {
+    if (optionsParam && optionsParam.type === 'SSML') {
+        return {
+            type: optionsParam.type,
+            ssml: optionsParam.speech
+        };
+    } else {
+        return {
+            type: optionsParam.type || 'PlainText',
+            text: optionsParam.speech || optionsParam
+        }
+    }
+}
+
+Response.prototype = (function () {
+    var buildSpeechletResponse = function (options) {
+        var alexaResponse = {
+            outputSpeech: createSpeechObject(options.output),
+            shouldEndSession: options.shouldEndSession
+        };
+        if (options.reprompt) {
+            alexaResponse.reprompt = {
+                outputSpeech: createSpeechObject(options.reprompt)
+            };
+        }
+        if (options.cardTitle && options.cardContent) {
+            alexaResponse.card = {
+                type: "Simple",
+                title: options.cardTitle,
+                content: options.cardContent
+            };
+        }
+        var returnResult = {
+            version: '1.0',
+            response: alexaResponse
+        };
+        if (options.session && options.session.attributes) {
+            returnResult.sessionAttributes = options.session.attributes;
+        }
+        return returnResult;
+    };
+
+    return {
+        tell: function (speechOutput) {
+            this._context.succeed(buildSpeechletResponse({
+                session: this._session,
+                output: speechOutput,
+                shouldEndSession: true
+            }));
+        },
+        tellWithCard: function (speechOutput, cardTitle, cardContent) {
+            this._context.succeed(buildSpeechletResponse({
+                session: this._session,
+                output: speechOutput,
+                cardTitle: cardTitle,
+                cardContent: cardContent,
+                shouldEndSession: true
+            }));
+        },
+        ask: function (speechOutput, repromptSpeech) {
+            this._context.succeed(buildSpeechletResponse({
+                session: this._session,
+                output: speechOutput,
+                reprompt: repromptSpeech,
+                shouldEndSession: false
+            }));
+        },
+        askWithCard: function (speechOutput, repromptSpeech, cardTitle, cardContent) {
+            this._context.succeed(buildSpeechletResponse({
+                session: this._session,
+                output: speechOutput,
+                reprompt: repromptSpeech,
+                cardTitle: cardTitle,
+                cardContent: cardContent,
+                shouldEndSession: false
+            }));
+        }
+    };
+})();
+
+
+/**
+ * App ID for the skill
+ */
+var APP_ID = "amzn1.echo-sdk-ams.app.107860b7-769b-4ada-bcaa-9021f1198ef2";
+
+/**
+ * The AlexaSkill prototype and helper functions
+ */
+
+/**
+ * HelloWorld is a child of AlexaSkill.
+ * To read more about inheritance in JavaScript, see the link below.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
+ */
+var HelloWorld = function () {
+    AlexaSkill.call(this, APP_ID);
+};
+
+// Extend AlexaSkill
+HelloWorld.prototype = Object.create(AlexaSkill.prototype);
+HelloWorld.prototype.constructor = HelloWorld;
+
+HelloWorld.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+    console.log("HelloWorld onSessionStarted requestId: " + sessionStartedRequest.requestId
+        + ", sessionId: " + session.sessionId);
+// any initialization logic goes here
+};
+
+HelloWorld.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    console.log("HelloWorld onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+    var speechOutput = "Welcome to Personal Computer";
+    var repromptText = "Waiting for your command";
+    response.ask(speechOutput, repromptText);
+};
+
+HelloWorld.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+    console.log("HelloWorld onSessionEnded requestId: " + sessionEndedRequest.requestId
+        + ", sessionId: " + session.sessionId);
+// any cleanup logic goes here
+};
+
+
+// MessageIntent
+// Map
+// Weather
+
+HelloWorld.prototype.intentHandlers = {
+// register custom intent handlers
+    "Selfie": function (intent, session, response) {
+        callApi("q=selfie", function (result) {
+        });
+        response.ask("", "Sending your selfie to all your friends.");
+    },
+    "WhereIsMyRoverOne": function (intent, session, response) {
+        callApi("q=whereismyroverone", function (result) {
+            response.ask("", "Rover one location within 300 meters. Battery status: 37%.  Oxygen supply 71%");
+        });
+    },
+    "WhereIsMyRoverTwo": function (intent, session, response) {
+        callApi("q=whereismyrovertwo", function (result) {
+            response.ask("", "Rover two got stolen last week");
+        });
+    },
+    "WhereAmI": function (intent, session, response) {
+        callApi("q=whereami", function (result) {
+            response.ask("", "You are on Mars");
+        });
+    },
+    "Direction": function (intent, session, response) {
+        callApi("q=direction", function (result) {
+            response.ask("", "Navigating to the hub.");
+        });
+    },
+    "MarsSurfaceTemperature": function (intent, session, response) {
+        callApi("q=marssurfacetemperature", function (result) {
+            response.ask("", result.data.response);
+        });
+    },
+    "Time": function (intent, session, response) {
+        callApi("q=time", function (result) {
+            response.ask("", result.data.response);
+        });
+    },
+    "Report": function (intent, session, response) {
+        callApi("q=report", function (result) {
+            response.ask("", result.data.response);
+        });
+    },
+    "HelloMars": function (intent, session, response) {
+        callApi("q=hellomars", function (result) {
+            response.ask("", result.data.response);
+        });
+    },
+    "MessageIntent": function (intent, session, response) {
+        callApi("q=massage", function (result) {
+            response.ask("", "Checking messages");
+        });
+    },
+    "Map": function (intent, session, response) {
+        callApi("q=map", function (result) {
+            response.ask("", "Showing map");
+        });
+    },
+    "Weather": function (intent, session, response) {
+        callApi("q=weather", function (result) {
+            response.ask("", "Loading weather data");
+        });
+    },
+    "AMAZON.HelpIntent": function (intent, session, response) {
+        response.ask("", "Please contact Mars Help Desk on channel alpha alpha one seven nine. Estimated wait time is 4 hours and 31 minutes. All consultants are busy.");
+    }
+
+};
+
+// Create the handler that responds to the Alexa Request.
+exports.handler = function (event, context) {
+// Create an instance of the HelloWorld skill.
+    var helloWorld = new HelloWorld();
+    helloWorld.execute(event, context);
+};
+
+
+//*****************
+var http = require('http');
+
+function callApi(params, callback) {
+    return http.get({
+        host: 'spaceappshackathon.eu-gb.mybluemix.net',
+        path: '/index.php?' + params
+    }, function (response) {
+// Continuously update stream with data
+        var body = '';
+        response.on('data', function (d) {
+            body += d;
+        });
+        response.on('end', function () {
+
+// Data reception is done, do whatever with it!
+            var parsed = JSON.parse(body);
+            callback(parsed);
+        });
+    });
+}
